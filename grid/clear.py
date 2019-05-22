3ad3f1a34e056d6e119f0063673bc87a02e4c2d1
Mario Geiger <geiger.mario@gmail.com>
2019-05-22 08:49:37 +0200

    clear rm clones

--- grid/clear.py
+++ grid/clear.py
@@ -12,15 +12,28 @@ def main():
     parser.add_argument("log_dir", type=str)
     args = parser.parse_args()
 
+    def tup(args):
+        d = [(key, value) for key, value in args.__dict__.items() if key != 'pickle']
+        return tuple(sorted(d))
+
+    done = set()
 
     for path in sorted(glob.glob("{}/*.pkl".format(args.log_dir))):
         with open(path, 'rb') as f:
-            torch.load(f)
+            args = tup(torch.load(f))
             try:
                 torch.load(f, map_location='cpu')
             except EOFError:
-                print("remove {}".format(path))
+                print("rm empty: {}".format(path))
                 os.remove(path)
+                continue
+
+            if args in done:
+                print("rm clone: {}".format(path))
+                os.remove(path)
+                continue
+
+            done.add(args)
 
 
 if __name__ == '__main__':
