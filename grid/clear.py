0feb6f5b4c48561a0f5af5cb7a9fab8fc6e33b7b
Mario Geiger <geiger.mario@gmail.com>
2019-10-04 12:01:45 +0200

    clear

--- grid/clear.py
+++ grid/clear.py
@@ -1,4 +1,4 @@
-# pylint: disable=C
+# pylint: disable=eval-used, missing-docstring, invalid-name
 import argparse
 import glob
 import os
@@ -15,7 +15,12 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("log_dir", type=str)
+    parser.add_argument("pred_args", type=str)
+    parser.add_argument("pred_run", type=str)
     args = parser.parse_args()
+
+    pred_args = eval(args.pred_args) if args.pred_args else None
+    pred_run = eval(args.pred_run) if args.pred_run else None
 
     def tup(args):
         d = [(key, value) for key, value in args.__dict__.items() if key != 'pickle']
@@ -25,14 +30,26 @@ def main():
 
     for path in tqdm(sorted(glob.glob("{}/*.pkl".format(args.log_dir)))):
         with open(path, 'rb') as f:
-            args = tup(torch.load(f))
+            args = torch.load(f)
+
+            if pred_args and not pred_args(args):
+                print("pred_args failed: {}".format(path))
+                os.remove(path)
+                continue
+
             try:
-                torch.load(f, map_location='cpu')
+                run = torch.load(f, map_location='cpu')
             except EOFError:
                 print("rm empty: {}".format(path))
                 os.remove(path)
                 continue
 
+            if pred_run and not pred_run(run):
+                print("pred_run failed: {}".format(path))
+                os.remove(path)
+                continue
+
+            args = tup(args)
             if args in done:
                 print("rm clone: {}".format(path))
                 os.remove(path)
