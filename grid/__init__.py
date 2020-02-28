6c0a57274235268a66daa4675c8b3ec6087af82e
Mario Geiger <geiger.mario@gmail.com>
2020-02-28 13:48:30 +0100

    args_intersection

--- grid/__init__.py
+++ grid/__init__.py
@@ -2,3 +2,32 @@
 from .load import load, load_iter
 from .info import print_info
 from .gpu import get_free_gpus
+
+
+def args_intersection(argss):
+    from functools import reduce
+
+    def hashable(x):
+        if isinstance(x, list):
+            x = tuple(x)
+        if isinstance(x, set):
+            x = frozenset(x)
+        try:
+            hash(x)
+        except TypeError:
+            return '<not hashable>'
+        return x
+
+    argss = [
+        {
+            key: hashable(value)
+            for key, value in r.__dict__.items()
+            if key != 'pickle'
+        }
+        for r in argss
+    ]
+
+    keys = reduce(set.intersection, [set(r.keys()) for r in argss])
+
+    values = {k: {r[k] for r in argss} for k in keys}
+    return {k: list(v)[0] for k, v in values.items() if len(v) == 1}
