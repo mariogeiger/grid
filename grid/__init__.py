02e7ec7ad15dac69e3a03b71ff61ee03265cfc4f
Mario Geiger <geiger.mario@gmail.com>
2020-03-06 20:15:23 +0100

    args_variation

--- grid/__init__.py
+++ grid/__init__.py
@@ -4,7 +4,7 @@ from .info import print_info
 from .gpu import get_free_gpus
 
 
-__all__ = ['load', 'load_iter', 'print_info', 'get_free_gpus', 'args_intersection']
+__all__ = ['load', 'load_iter', 'print_info', 'get_free_gpus', 'args_intersection', 'args_variation']
 
 
 def hashable(x):
