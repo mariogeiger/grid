021aae8ba14d890b47b8906d78d4f13c4d323856
Mario Geiger <geiger.mario@gmail.com>
2020-03-06 20:14:36 +0100

    args_variation

--- grid/__init__.py
+++ grid/__init__.py
@@ -7,19 +7,20 @@ from .gpu import get_free_gpus
 __all__ = ['load', 'load_iter', 'print_info', 'get_free_gpus', 'args_intersection']
 
 
+def hashable(x):
+    if isinstance(x, list):
+        x = tuple(x)
+    if isinstance(x, set):
+        x = frozenset(x)
+    try:
+        hash(x)
+    except TypeError:
+        return '<not hashable>'
+    return x
+
+
 def args_intersection(argss):
     from functools import reduce
-
-    def hashable(x):
-        if isinstance(x, list):
-            x = tuple(x)
-        if isinstance(x, set):
-            x = frozenset(x)
-        try:
-            hash(x)
-        except TypeError:
-            return '<not hashable>'
-        return x
 
     argss = [
         {
@@ -34,3 +35,21 @@ def args_intersection(argss):
 
     values = {k: {r[k] for r in argss} for k in keys}
     return {k: list(v)[0] for k, v in values.items() if len(v) == 1}
+
+
+def args_variation(argss):
+    argss = [
+        {
+            key: hashable(value)
+            for key, value in r.__dict__.items()
+            if key != 'pickle'
+        }
+        for r in argss
+    ]
+
+    keys = {key for r in argss for key in r.keys()}
+
+    return {
+        key: {r[key] if key in r else None for r in argss}
+        for key in keys
+    }
