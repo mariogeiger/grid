7d38da0c82257eb632b0a18ddd1aa4c68a2b779a
Mario Geiger <geiger.mario@gmail.com>
2020-10-26 23:26:44 +0100

    merge

--- grid/merge.py
+++ grid/merge.py
@@ -5,7 +5,7 @@ import os
 import random
 from itertools import count
 
-import torch
+from grid.exec import load_args, load_data
 
 
 def main():
@@ -13,26 +13,41 @@ def main():
 
     parser.add_argument("log_dir_src", type=str)
     parser.add_argument("log_dir_dst", type=str)
+    parser.add_argument("--pred_args", type=str)
+    parser.add_argument("--pred_run", type=str)
     args = parser.parse_args()
 
+    pred_args = eval(args.pred_args) if args.pred_args else None
+    pred_run = eval(args.pred_run) if args.pred_run else None
+
     def tup(args):
-        d = [(key, value) for key, value in args.__dict__.items() if key != 'pickle']
+        d = [(key, value) for key, value in args.__dict__.items() if key not in ['pickle', 'output']]
         return tuple(sorted(d))
 
     done_dst = {
-        tup(torch.load(f))
-        for f in glob.glob("{}/*.pkl".format(args.log_dir_dst))
+        tup(load_args(f))
+        for f in glob.glob("{}/*.pk".format(args.log_dir_dst))
     }
 
-    for path_src in glob.glob("{}/*.pkl".format(args.log_dir_src)):
-        args_src = tup(torch.load(path_src))
+    for path_src in glob.glob("{}/*.pk".format(args.log_dir_src)):
+        a = load_args(path_src)
+        if pred_args is not None and not pred_args(a):
+            print("{} skipped".format(path_src))
+            continue
+
+        if pred_run is not None and not pred_run(load_data(path_src)):
+            print("{} skipped".format(path_src))
+            continue
+
+        args_src = tup(a)
 
         if args_src in done_dst:
             print("{} ok".format(path_src))
             continue
 
-        for i in count(random.randint(0, 50000)):
-            name = "{:05d}.pkl".format(i)
+        for i in count(random.randint(0, 999_999)):
+            i = i % 1_000_000
+            name = "{:05d}.pk".format(i)
             path_dst = os.path.join(args.log_dir_dst, name)
             if not os.path.isfile(path_dst):
                 break
