c87dc9d4dacf282a78fffa6306750c98b40e9177
Mario Geiger <geiger.mario@gmail.com>
2021-08-15 18:37:47 +0200

    group_runs

--- grid/__init__.py
+++ grid/__init__.py
@@ -1,9 +1,9 @@
 from ._args import to_dict, args_diff, args_hash_able, args_intersection, args_union, keyall, hash_able
-from ._io import load, load_file, load_grouped, load_iter, load_args
+from ._io import load, load_file, load_grouped, load_iter, load_args, group_runs
 from ._exec import exec_grid, exec_one, new_filename
 
 __all__ = [
     "to_dict", "args_diff", "args_hash_able", "args_intersection", "args_union", "keyall", "hash_able",
-    "load", "load_file", "load_grouped", "load_iter", "load_args",
+    "load", "load_file", "load_grouped", "load_iter", "load_args", "group_runs",
     "exec_grid", "exec_one", "new_filename",
 ]
